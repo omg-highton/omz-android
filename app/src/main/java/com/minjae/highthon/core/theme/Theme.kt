3544559a08e4015699b857adc914bec5a2d4bf94
최민재 <cmjcmj1230@gmail.com>
2023-01-14 22:25:30 +0900

    Add : Omz Theme

--- app/src/main/java/com/minjae/highthon/core/theme/Theme.kt
+++ app/src/main/java/com/minjae/highthon/core/theme/Theme.kt
@@ -1,34 +1,45 @@
 package com.minjae.highthon.core.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+@SuppressLint("ConflictingOnColor")
+private val LightColorPalette = lightColors(
+    primary = OmzColor.MainColor,
+    secondary = OmzColor.SubColor,
+    background = OmzColor.BackgroundColor,
+    surface = Color.White,
+    error = Color.Red,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.White,
+    onError = Color.Red
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
+private val DarkColorPalette = darkColors(
+    primary = OmzColor.MainColor,
+    secondary = OmzColor.SubColor,
+    background = OmzColor.BackgroundColor,
     surface = Color.White,
+    error = Color.Red,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    onError = Color.Red
 )
 
 @Composable
-fun HighthonTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun OmzTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+     content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
@@ -37,7 +48,6 @@ fun HighthonTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
         shapes = Shapes,
         content = content
     )
