4440a3906e0095523b5f70536c422a37351dba2e
limsaehyun <sh00710000@gmail.com>
2023-01-15 06:27:21 +0900

    feat: home fake2

--- app/src/main/java/com/minjae/highthon/features/chat/screen/ChatScreen.kt
+++ app/src/main/java/com/minjae/highthon/features/chat/screen/ChatScreen.kt
@@ -1,20 +1,33 @@
 package com.minjae.highthon.features.chat.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.minjae.highthon.R
 import com.minjae.highthon.core.theme.OmzColor
 
 @Composable
 fun ChatScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(OmzColor.Check)
+            .verticalScroll(rememberScrollState())
     ) {
-        Text(text = "채팅")
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(id = R.drawable.bg_chat),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+        )
     }
 }
--- app/src/main/java/com/minjae/highthon/features/myinfo/screen/MyInfoScreen.kt
+++ app/src/main/java/com/minjae/highthon/features/myinfo/screen/MyInfoScreen.kt
@@ -1,20 +1,33 @@
 package com.minjae.highthon.features.myinfo.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.minjae.highthon.R
 import com.minjae.highthon.core.theme.OmzColor
 
 @Composable
 fun MyInfoScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(OmzColor.Check)
+            .verticalScroll(rememberScrollState())
     ) {
-        Text(text = "내정보")
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(id = R.drawable.bg_my_page),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+        )
     }
 }
